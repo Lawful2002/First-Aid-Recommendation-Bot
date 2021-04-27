859ce25607c2314424b248eac9f94110d96325c7
Ayushsunny <ayush100anand@gmail.com>
2021-04-27 19:12:22 +0530

    tested

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     return src.chat(userText)
-    # return str(CB.get_response(userText))
+    
 app.run(debug = True)
